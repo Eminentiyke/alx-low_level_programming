7ffcf33b99cbee39fbe70f1f3134f496aa1f3b38
eminent <nwachi.iyke2017@gmail.com>
2022-09-19 14:56:58 -0700

    task2bv

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
-#include "main.h"
-
-/**
- * _strlen - Returns the length of a string.
- * @str: string.
- *
- * Return: Length.
- */
-size_t _strlen(const char *str)
-{
-	size_t len = 0;
-
-	while (*str++)
-		len++;
-
-	return (len);
-}
+/**                                                             
+ * _strlen - returns the length of a string                     
+ *                                                              
+ * @s: string parameter input                                   
+ *                                                              
+ * Return: length of string                                     
+*/                                                              
+                                                                
+int _strlen(char *s)                                            
+{                                                               
+        int counter;                                            
+                                                                
+        for (counter = 0; *s != '\0'; ++s)                      
+                ++counter;                                      
+                                                                
+        return (counter);                                       
+} 
